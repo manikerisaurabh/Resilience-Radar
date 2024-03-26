a67c33c60ae91d60a9018805e2abfab749c80075
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-26 19:47:41 +0530

    login route optimized

--- Backend/app.js
+++ Backend/app.js
@@ -129,6 +129,8 @@ app.get("/login", (req, res) => {
 
 app.post('/login', async (req, res) => {
     let { email, password } = req.body;
+    console.log(req.body);
+    return;
     try {
         let users = await User.find({ email: email });
 
