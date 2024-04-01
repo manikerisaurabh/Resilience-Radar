918038eb084cc4e16b4246802f101e7e410eb5b4
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-02 00:07:07 +0530

    added isvalid middlerware to verify user

---
+++ Backend/middlewares/isValidUser.js
@@ -0,0 +1,11 @@
+import User from "../models/user.js";
+
+export const isValidUser = async (req, res, next) => {
+    let { id } = req.params;
+    let isValidUser = await User.findById(id);
+    if (isValidUser.length == 0) {
+        return res.status(200).json({ error: "invalid user" });
+    }
+
+    next();
+}
