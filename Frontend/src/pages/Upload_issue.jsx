f5cb90a08c481759ed12b55fd880d1caeb314677
SpAkS <sairajmane2004@gmail.com>
2024-04-16 00:51:42 +0530

    minor changes and backend-frontent linking part-1

--- Frontend/src/pages/Upload_issue.jsx
+++ Frontend/src/pages/Upload_issue.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import WebcamCapture from '../Components/Media/WebcamCapture ';
 import QueryForm from '../Components/Form/QueryForm';
-import MapComponent from '../Components/Maps/MapComponent';
 
 const Upload_issue = () => {
   const [imageUrl, setImageUrl] = useState("");
