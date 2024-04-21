d112d1ee00ae7ed1ca5f558fee5ccbd3e16f1d66
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-21 10:41:54 +0530

    added pending query route

--- Backend/routes/query.route.js
+++ Backend/routes/query.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addQuery, allQueries, editQuery, getEditQueryInfo } from "../controller/query.controller.js";
+import { addQuery, allQueries, editQuery, getEditQueryInfo, pendingQueries } from "../controller/query.controller.js";
 import { isValidUser } from "../middlewares/isValidUser.js";
 
 const router = express.Router();
@@ -11,4 +11,7 @@ router.post("/add/:id", isValidUser, addQuery);
 router.get("/edit/:id", getEditQueryInfo);
 
 router.put("/edit/:id", isValidUser, editQuery);
+
+router.get("/pending/:id", isValidUser, pendingQueries);
+
 export default router;
